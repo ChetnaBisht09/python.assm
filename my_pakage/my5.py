32a6dbdba96f4c4c714fe488de713639d87efcd7
ChetnaBisht09 <bishtchetna06@gmail.com>
2025-05-05 21:42:38 +0530

    Update my5.py

--- my_pakage/my5.py
+++ my_pakage/my5.py
@@ -1,7 +1,3 @@
-import sys
-import os
-'my_package'
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"my_package")))
 
 import my_package.list1 as l
 import my_package.set2 as s
