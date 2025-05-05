fd605b59444bd6e05bcd2ea3e579f74ea8ba51a0
ChetnaBisht09 <bishtchetna06@gmail.com>
2025-05-05 08:43:59 +0530

    Add files via upload

---
+++ m4.py
@@ -0,0 +1,18 @@
+import list1
+import set2
+import dict3
+
+list1.append1(5)
+list1.extend1([6,7])
+list1.pop()
+list1.remove1(6)
+
+set2.adds(10)
+set2.adds(20)
+set2.slen2()
+set2.remove2(10)
+
+dict3.add3("name","alice")
+dict3.len()
+dict3.keys()
+dict3.values3()
---
+++ m5.py
@@ -0,0 +1,12 @@
+# m5.py
+
+import cal1
+
+a = 10
+b = 3
+
+print("Addition:", cal1.add1(a, b))
+print("Multiplication:", cal1.mul1(a, b))
+print("Division:", cal1.div1(a, b))
+print("Modulus:", cal1.mod1(a, b))
+print("Power:", cal1.pow1(a, b))
